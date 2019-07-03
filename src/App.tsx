ad191b09e98ec227efa6132a8e2dc894ad9694f2
Daniel <dnljara2392@gmail.com>
2019-07-03 07:04:37 -0500

    Single page navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from './components/Home'
+import Other from './components/Other'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Simple SPA</h1>
-      <ul className="header">
-        <li><a href="/">Home</a></li>
-        <li><a href="/stuff">Stuff</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-      <div className="content">
-        
+    <HashRouter>
+      <div className="App">
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/Other">Other</NavLink></li>
+        </ul>
+        <div className="content">
+          <Route exact path="/" component={Home}/>
+          <Route path="/Other" component={Other}/>
+        </div>
       </div>
-    </div>
+    </HashRouter>
   );
 }
 
---
+++ src/components/Other.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Other:React.FC = () => {
+    return(
+        <div>
+        <h2>Others</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas odio quidem eius quibusdam, natus alias enim sunt labore reiciendis in itaque, quaerat unde ullam, exercitationem sequi error quae distinctio facere.</p>
+      </div>
+    )
+}
+
+export default Other
