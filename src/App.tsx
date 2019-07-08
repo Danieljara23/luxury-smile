f84544cc7ccf1b454d5e40b6be769207ed9669ec
Daniel <dnljara2392@gmail.com>
2019-07-07 23:39:52 -0500

    Navbar is ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 import {
   Route,
-  NavLink,
   HashRouter
 } from "react-router-dom";
+import NavBar from './shared/Navbar/NavBar'
 import Home from './components/Home'
 import Other from './components/Other'
 
@@ -12,11 +12,7 @@ const App: React.FC = () => {
   return (
     <HashRouter>
       <div className="App">
-        <h1>Simple SPA</h1>
-        <ul className="header">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/Other">Other</NavLink></li>
-        </ul>
+        <NavBar/>
         <div className="content">
           <Route exact path="/" component={Home}/>
           <Route path="/Other" component={Other}/>
