c3873ff7dc202a4d5158fcf0973f75994be6590b
Daniel <dnljara2392@gmail.com>
2019-07-15 23:20:48 -0500

    Especialist Carousel modified

--- src/components/Home/HomeComponents/Especialist.tsx
+++ src/components/Home/HomeComponents/Especialist.tsx
@@ -2,35 +2,58 @@ import React, {useState} from 'react';
 import { EspecialistsList } from './EspecialistList'
 import { ReactComponent as IcoCalidad } from '../../images/ico-calidad.svg'
 import { ReactComponent as Flecha } from '../../images/ico-flecha.svg'
-import ItemsCarousel from 'react-items-carousel';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const isMobile = window.innerWidth < 600;
 
 const RenderList = () => {
     console.log(EspecialistsList)
     const setWidth = window.innerWidth - 10 
+    
     return(
         <>
-        {EspecialistsList.map((item)=> (
-            <div className={`${ isMobile ? 'w-30 mobile-card':'w-30'}`} style={{width: setWidth}}>
-                <div className="especialist-image">
-                    <img src={item.imgPath} alt={item.name}/>
+            {EspecialistsList.map((item)=>
+                <div className={`${ isMobile ? '':'w-30'}`} >
+                    <div className="especialist-image">
+                        <img src={item.imgPath} alt={item.name}/>
+                    </div>
+                    <div className="especialist-info mt3 flex flex-column justify-center items-center">
+                        <p className="especialist-name"><span className="especialist-prefix">{item.prefixType}.</span> {item.name}</p>
+                        <p className="especialist-school">{item.schoolName}</p>
+                        <div className="quality-icon"><IcoCalidad/></div>
+                        <p className="especialist-treatment">{item.fieldName}</p>
+                        <p className="especialist-read-more">Leer más <Flecha/></p>
+                    </div>
                 </div>
-                <div className="especialist-info mt3 flex flex-column justify-center items-center">
-                    <p className="especialist-name"><span className="especialist-prefix">{item.prefixType}.</span> {item.name}</p>
-                    <p className="especialist-school">{item.schoolName}</p>
-                    <div className="quality-icon"><IcoCalidad/></div>
-                    <p className="especialist-treatment">{item.fieldName}</p>
-                    <p className="especialist-read-more">Leer más <Flecha/></p>
-                </div>
-            </div>
-        ))}
+            )}
+        )
         </>
     )
+    
 }
 
 
 const Especialist = () => {
+    const [galleryItems, setGalleryItems] = useState(
+        EspecialistsList.map((i) => (
+            <div className={`${ isMobile ? '':'w-30'}`}  >
+                <div className="especialist-image">
+                    <img src={i.imgPath} alt={i.name}/>
+                </div>
+                <div className="especialist-info mt3 flex flex-column justify-center items-center">
+                    <p className="especialist-name"><span className="especialist-prefix">{i.prefixType}.</span> {i.name}</p>
+                    <p className="especialist-school">{i.schoolName}</p>
+                    <div className="quality-icon"><IcoCalidad/></div>
+                    <p className="especialist-treatment">{i.fieldName}</p>
+                    <p className="especialist-read-more">Leer más <Flecha/></p>
+                </div>
+            </div>
+        ))
+
+    )
+    const handleOnDragStart = (e:any) => e.preventDefault()
+   
     
     const [activeItemIndex, setActiveItemIndex] = useState(0);
     return(
@@ -41,11 +64,15 @@ const Especialist = () => {
             <div className="w-100 flex justify-center especialist-container">
                 {
                     isMobile ? (
-                        <div className="w-100 treatment-container-mobile">
-                            <RenderList />
-                        </div>
+                        <AliceCarousel mouseDragEnabled
+                            items={galleryItems} 
+                        />
+                           
+                        
+
+                        
                     ):(
-                        <RenderList />
+                        <RenderList/>
                     )
                 }
                
