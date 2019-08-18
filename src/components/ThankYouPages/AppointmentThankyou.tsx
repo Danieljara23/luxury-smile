0f359906a758ea3e99a80fd64907d7d29b797af3
Daniel <dnljara2392@gmail.com>
2019-08-18 18:29:40 -0500

    Auto stash before revert of "Final adds to GA config"

--- src/components/ThankYouPages/AppointmentThankyou.tsx
+++ src/components/ThankYouPages/AppointmentThankyou.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import '../Home/styles.css'
 import './Thankyou.css'
+import {withRouter} from 'react-router-dom';
+import TagManager from 'react-gtm-module'
 
-
+const tagManagerArgs = {
+    dataLayer: {
+        userId: '001',
+        userProject: 'project',
+        page: 'appointment-thank-you'
+    },
+    dataLayerName: 'PageDataLayer'
+}
 const AppointmentThankyou = () => {
+    TagManager.dataLayer(tagManagerArgs)
     return(
         <div className="thank-you-page">
             <h4 className="appointment-title">Hemos agendado tu cita</h4>
@@ -15,5 +25,5 @@ const AppointmentThankyou = () => {
     )
 }
 
-export default AppointmentThankyou
+export default withRouter(AppointmentThankyou)
 
